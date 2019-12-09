0cffae563e0a07b8416ac8d65e271e9643089cb8
AdrienCos <ad.cosson@gmail.com>
2019-12-08 20:15:06 -0500

    Added arg parsing

--- intruder_client.py
+++ intruder_client.py
@@ -3,6 +3,7 @@
 import socket
 import time
 import config as cfg
+import argparse
 
 
 def start_attack(attack_type: int, start: int, duration: int):
@@ -17,4 +18,14 @@ def start_attack(attack_type: int, start: int, duration: int):
 
 
 if __name__ == "__main__":
-    start_attack(0, 1, 10)
+    parser = argparse.ArgumentParser(
+        description='Details of the attack to start')
+    parser.add_argument('-t', type=int, default=0, dest="attack_type",
+                        help="Attack type (defaults to 0)")
+    parser.add_argument('-s', type=int, default=0, dest="start",
+                        help="Start time offet (defaults to 0)")
+    parser.add_argument('-d', type=int, default=60, dest="duration",
+                        help='Attack duration (defaults to 60s)')
+
+    args = parser.parse_args()
+    start_attack(args.attack_type, args.start, args.duration)
